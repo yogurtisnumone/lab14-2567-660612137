bf6e558e789e7bae7985e85e2a0f115b931e62f8
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-09-07 16:02:15 +0700

    11!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
         <Pagination total={20} color="orange" />
       </Group>
 
-      <Text c="rgb(134 , 142 , 150)">
+      <Text ta="center" my="sm" c="rgb(134 , 142 , 150)">
         <Footer
           year={2024}
           name="Kachapat Punthong"
