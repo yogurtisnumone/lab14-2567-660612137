fb75da69e10e72dab72401e4825d6d4c91626f72
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-09-07 16:00:45 +0700

    11!

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer( props:any ) {
   return (
-    <div style={{ textAlign: "center", marginTop: "15px"  }}>
+    <div>
       <p>Copyright © {props.year} {props.name} {props.id} </p>
     </div>
   );
